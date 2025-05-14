eec5e4ce288b0144099e96c8ae8b44036a13dcbc
Amrin002 <amrinlatuconsina@gmail.com>
2025-05-15 05:48:09 +0900

    "update: Hosting ke ngrok, semua asset diganti jadi secure_asset"

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -9,8 +9,8 @@
     <meta name="keywords" content="">
 
     <!-- Favicons -->
-    <link href="{{ asset('landing/assets/img/favicon.png') }}" rel="icon">
-    <link href="{{ asset('landing/assets/img/apple-touch-icon.png') }}" rel="apple-touch-icon">
+    <link href="{{ secure_asset('landing/assets/img/favicon.png') }}" rel="icon">
+    <link href="{{ secure_asset('landing/assets/img/apple-touch-icon.png') }}" rel="apple-touch-icon">
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com" rel="preconnect">
@@ -20,17 +20,17 @@
         rel="stylesheet">
 
     <!-- Vendor CSS Files -->
-    <link href="{{ asset('landing/assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('landing/assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
-    <link href="{{ asset('landing/assets/vendor/aos/aos.css') }}" rel="stylesheet">
-    <link href="{{ asset('landing/assets/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('landing/assets/vendor/glightbox/css/glightbox.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('landing/assets/vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('landing/assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('landing/assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('landing/assets/vendor/aos/aos.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('landing/assets/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('landing/assets/vendor/glightbox/css/glightbox.min.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('landing/assets/vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
 
 
 
     <!-- Main CSS File -->
-    <link href="{{ asset('landing/assets/css/main.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('landing/assets/css/main.css') }}" rel="stylesheet">
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/orgchart/3.8.0/css/jquery.orgchart.css">
     <style>
@@ -67,7 +67,7 @@
 
             <div class="container position-relative d-flex align-items-center justify-content-end">
                 <a href="index.html" class="logo d-flex align-items-center me-auto">
-                    <img src="{{ asset('landing/assets/img/Logo2.png') }}" alt="">
+                    <img src="{{ secure_asset('landing/assets/img/Logo2.png') }}" alt="">
                     <!-- Uncomment the line below if you also wish to use a text logo -->
                     <h1 class="sitename">Akat Fadedo</h1>
                 </a>
@@ -180,17 +180,18 @@
             </div>
         </div> --}}
 
-        <div class="container copyright text-center mt-4">
-            <p>© <span>Copyright</span> <strong class="px-1 sitename">Local Class Techonology</strong> <span>All Rights
-                    Reserved</span></p>
-            <div class="credits">
-                <!-- All the links in the footer should remain intact. -->
-                <!-- You can delete the links only if you've purchased the pro version. -->
-                <!-- Licensing information: https://bootstrapmade.com/license/ -->
-                <!-- Purchase the pro version with working PHP/AJAX contact form: [buy-url] -->
-                {{-- Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a> Distributed by <a href=“https://themewagon.com>ThemeWagon --}}
-            </div>
-        </div>
+                <div class="container copyright text-center mt-4">
+                    <p>© <span>Copyright</span> <strong class="px-1 sitename">Local Class Techonology</strong> <span>All
+                            Rights
+                            Reserved</span></p>
+                    <div class="credits">
+                        <!-- All the links in the footer should remain intact. -->
+                        <!-- You can delete the links only if you've purchased the pro version. -->
+                        <!-- Licensing information: https://bootstrapmade.com/license/ -->
+                        <!-- Purchase the pro version with working PHP/AJAX contact form: [buy-url] -->
+                        {{-- Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a> Distributed by <a href=“https://themewagon.com>ThemeWagon --}}
+                    </div>
+                </div>
 
     </footer>
 
@@ -202,15 +203,15 @@
     <div id="preloader"></div>
 
     <!-- Vendor JS Files -->
-    <script src="{{ asset('landing/assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-    <script src="{{ asset('landing/assets/vendor/php-email-form/validate.js') }}"></script>
-    <script src="{{ asset('landing/assets/vendor/aos/aos.js') }}"></script>
-    <script src="{{ asset('landing/assets/vendor/glightbox/js/glightbox.min.js') }}"></script>
-    <script src="{{ asset('landing/assets/vendor/purecounter/purecounter_vanilla.js') }}"></script>
-    <script src="{{ asset('landing/assets/vendor/swiper/swiper-bundle.min.js') }}"></script>
+    <script src="{{ secure_asset('landing/assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ secure_asset('landing/assets/vendor/php-email-form/validate.js') }}"></script>
+    <script src="{{ secure_asset('landing/assets/vendor/aos/aos.js') }}"></script>
+    <script src="{{ secure_asset('landing/assets/vendor/glightbox/js/glightbox.min.js') }}"></script>
+    <script src="{{ secure_asset('landing/assets/vendor/purecounter/purecounter_vanilla.js') }}"></script>
+    <script src="{{ secure_asset('landing/assets/vendor/swiper/swiper-bundle.min.js') }}"></script>
 
     <!-- Main JS File -->
-    <script src="{{ asset('landing/assets/js/main.js') }}"></script>
+    <script src="{{ secure_asset('landing/assets/js/main.js') }}"></script>
 
 
     {{--
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,13 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        host: true,        // penting agar bisa diakses dari luar
+        strictPort: true,  // agar port tetap stabil
+        hmr: {
+            host: 'localhost', // bisa juga diganti IP lokal kamu
+        },
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
