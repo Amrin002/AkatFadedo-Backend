bb4541a01abb00b4d3d7eb356018cc6f0040496a
Amrin002 <amrinlatuconsina@gmail.com>
2025-09-08 18:09:57 +0900

    fix(api): correct case in AuthController import

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AppVersionApiController;
-use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\BeritaApiController;
 use App\Http\Controllers\Api\PasswordResetApiController;
 use App\Http\Controllers\Api\SuratPindahApiController;
