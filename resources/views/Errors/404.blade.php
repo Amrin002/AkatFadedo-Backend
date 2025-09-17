bc4032ff64b850ea9cdccdd54806b3e1ce8c39d0
Dean117 <audio.jordan17@gmail.com>
2025-09-17 23:06:52 +0900

    Fix 404.blade

--- resources/views/Errors/404.blade.php
+++ resources/views/Errors/404.blade.php
@@ -3,10 +3,21 @@
 @section('title', 'Halaman Tidak Ditemukan')
 
 @section('content')
-    <div style="text-align: center; padding: 100px; height:auto">
-        <img src="{{ asset('images/notfound.png') }}" alt="404 Not Found" style="max-width: 350px; width: 100%; height: auto;">
-        <h3>Oops! Halaman tidak ditemukan.</h3>
-        <p>Sepertinya halaman yang kamu cari tidak tersedia.</p>
-        <a href="{{ url('/') }}" class="px-4 btn btn-outline-primary rounded-pill">Kembali ke Beranda</a>
-    </div>
+    <main>
+        <div class="text-center d-flex flex-column justify-content-center align-items-center"
+            style="min-height: calc(100vh - 100px);">
+            <!-- 100px kira-kira tinggi header + footer -->
+
+            <img src="{{ asset('images/notfound.png') }}"
+                alt="404 Not Found"
+                style="max-width: 350px; width: 100%; height: auto;">
+
+            <h3 class="mt-4">Oops! Halaman tidak ditemukan.</h3>
+            <p>Sepertinya halaman yang kamu cari tidak tersedia.</p>
+
+            <a href="{{ url('/') }}" class="px-4 mt-3 btn btn-outline-primary rounded-pill">
+                Kembali ke Beranda
+            </a>
+        </div>
+    </main>
 @endsection
