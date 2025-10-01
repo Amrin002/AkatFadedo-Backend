b3a4d476668fbd7a0c5b6d9a38cb88e07244c5b7
Amrin002 <amrinlatuconsina@gmail.com>
2025-10-01 15:17:38 +0900

    feat: add UmkmSeeder to DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,7 @@ class DatabaseSeeder extends Seeder
             GaleriDesaSeeder::class,
             BeritaSeeder::class,
             ApbdesSeeder::class,
+            UmkmSeeder::class
         ]);
     }
 }
